93ee68b2fc08e924c34449704735da08e40d9384
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-25 10:34:05 +0800

    Update

---
+++ Day2.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+console.log("Welcome to our Wine Store");
+console.log("In order to but some wine you need to be over 18");
+
+UserInput = Number(prompt("How old are you: "));
+
+
+if (UserInput > 18) {
+    console.log("You are now legal to buy some wine");
+}else {
+    console.log("You are a minor and cannot buy wine. Sorry :) ");
+}
