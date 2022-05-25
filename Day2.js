74ec25e9d29183b2c47582e8be1b628df42213a8
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-25 10:40:10 +0800

    Update

--- Day2.js
+++ Day2.js
@@ -1,13 +1,23 @@
-const prompt = require('prompt-sync')({sigint: true});
+function ifLegal(){
 
-console.log("Welcome to our Wine Store");
-console.log("In order to but some wine you need to be over 18");
-
-UserInput = Number(prompt("How old are you: "));
-
-
-if (UserInput >= 18) {
-    console.log("You are now legal to buy some wine");
-}else {
-    console.log("You are a minor and cannot buy wine. Sorry :) ");
+    const prompt = require('prompt-sync')({sigint: true});
+    
+    console.log("Welcome to our Wine Store");
+    console.log("In order to but some wine you need to be over 18");
+    
+    UserInput = Number(prompt("How old are you: "));
+    
+    
+    if (UserInput >= 18) {
+        console.log("You are now legal to buy some wine");
+    }else {
+        console.log("You are a minor and cannot buy wine. Sorry :) ");
+    }
 }
+
+function getLargerInt(){
+    const prompt = require('prompt-sync')({sigint: true});
+
+    
+}
+    
