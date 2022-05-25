48277b05b86bbfc7b358765761314491fbd48f36
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-25 10:37:35 +0800

    Update

--- Day2.js
+++ Day2.js
@@ -6,7 +6,7 @@ console.log("In order to but some wine you need to be over 18");
 UserInput = Number(prompt("How old are you: "));
 
 
-if (UserInput > 18) {
+if (UserInput >= 18) {
     console.log("You are now legal to buy some wine");
 }else {
     console.log("You are a minor and cannot buy wine. Sorry :) ");
