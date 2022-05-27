75cd732dbbe7978936e3ec0057cc96e6eab84618
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-27 09:13:50 +0800

    new Day4.js file

---
+++ Day4.js
@@ -0,0 +1,52 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+
+function displaySum(prompt){
+
+    let sum = 0;
+
+    do {
+
+        input = prompt("Number: ");
+
+        sum += input;
+    } while(input >= 0)
+
+}
+
+function breakLoop(prompt){
+    let input = prompt("Number: ");
+
+    for (let i = 0; i < input; --i){
+        console.log("less than");
+        if (i == -10) {
+            break; 
+        }
+    }
+}
+
+function continueLoop(prompt){
+    let _input = 0;
+
+
+    do {
+        let input = prompt("Number: ");
+        _input = input;
+        console.log(_input);
+        if (input == 5){
+            continue;
+        }
+    } while(_input != 10)
+}
+
+
+function printValI(prompt){
+    let input = prompt("Number: ");
+    for (let i = 1; i <= input; ++i){
+        if (i == 5 || i == 10){
+            continue
+        }
+        console.log(i);
+    }
+}
+
