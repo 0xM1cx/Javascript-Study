c599d13eb83cec5497c9aa1d0e3c9f31c3a8c70b
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-28 11:25:44 +0800

    Update

--- Day5.js
+++ Day5.js
@@ -1,14 +1,16 @@
 const prompt = require("prompt-sync")({sigint: true});
 
 // Display Text
-function displayGreeting(prompt){
-    userName = prompt("What is your name? ");
+function displayGreeting(){
+    let userName = prompt("What is your name? ");
     return `Welcome ${userName}`;
 
-
+}
+// variable scope
+function getNameLog(){
+    let userName = prompt("What's you Username? "); //; using the prompt global variable
+    let date = Date.now();
+    console.log(`User: ${userName}, Logged in on ${date}`);
 }
 
-console.log(Date.now());
-greeting = displayGreeting(prompt);
-
-console.log(greeting);
+getNameLog();
