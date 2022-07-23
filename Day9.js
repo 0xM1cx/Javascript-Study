cf7f1e47b1336bcd512635fcd464655101c36f3c
Shawn <shawnlovecode14@gmail.com>
2022-07-23 20:33:46 +0800

    update

--- Day9.js
+++ Day9.js
@@ -1,10 +1,21 @@
+// All problems are found here: https://www.w3resource.com/javascript-exercises/javascript-recursion-functions-exercises.php
+
 const prompt = require('prompt-sync')();
 
+// question 1
 function calculateFactorial() {
+    let numberFactorial = 1;
     let userInput = prompt("Write a number to get the factorial: ");
     for(let i = 1; i <= userInput; ++i){
-        console.log("Number: ${i}");
+        numberFactorial *= i;
     }
+    return `The factorial of ${userInput} is : ${numberFactorial}`;
 }
 
-calculateFactorial();
+// question 2
+function findGCD(){
+    var numberOne = prompt("First positive number: ");
+    var numberTwo = prompt("Second positive number: ");
+
+    
+}
