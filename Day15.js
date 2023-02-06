a02396aec3bb8a99c30f639bcaaabeecb486ef49
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-02-06 23:38:03 +0800

    PROTOTYPE PRACTICE

---
+++ Day15.js
@@ -0,0 +1,14 @@
+function Car(){
+    this.color = "blue";
+}
+
+
+const suzuki = new Car();
+
+function Student(){
+    this.name = "Shawn",
+    this.age = 18
+}
+
+const BSIT_Student = Student();
+console.log(Student.prototype);
