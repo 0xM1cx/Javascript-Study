89aa71c42fad397324e9ee30e839cb5314a7c33d
shun <shawnlovecode14@gmail.com>
2023-03-20 22:17:31 +0800

    Learned Spread Operator in JS

---
+++ es6-spread-operator.js
@@ -0,0 +1,25 @@
+// The spread operator allows an iterable object to expand in places where more than zero arguments are expected
+
+// make my string into a array of characters
+function makeToArray(){
+
+    let name = "Shawn";
+    let chars = [...name];
+    
+    console.log(chars);
+}
+
+function arrayConcat(){
+    let names = ["Shawn", "Zyra", "Steven", "Mike"];
+    let added = [...names, "Sudaria", "Dauplo", "Sabalza"];
+
+    console.log(added[2]);
+}
+
+
+function misc(){
+    let names = ["Shawn", ..."Zyra", "Steven", "Mike"];
+    console.log(names);
+
+}
+
