324fbdfca9e1165f42ea2d48172ee89d14e1af64
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-06-28 08:23:03 +0800

    Experiement

--- Web-Proj/Vite-ts-react/blog-app/src/App.tsx
+++ Web-Proj/Vite-ts-react/blog-app/src/App.tsx
@@ -1,64 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useLayoutEffect, useState } from 'react'
 import './App.css'
 
 let user:{
-  name: string;
-  age: number;
-  isloggedIn: boolean;
+	name: string;
+	age: number;
+	isloggedIn: boolean;
 };
 
+user = {
+	name: "Shawn",
+  	age: 18,
+  	isloggedIn: false
+};
+
+
 function LoginPage() {
-  return (
-    <>
-      <h1>Please Login</h1>
-    </>
-  );  
+	const [cur, setval] = useState(0);
+	let goBack = () => {
+		return user.isloggedIn = true;
+	}
+	if (cur == 5){
+		return user.isloggedIn = true;
+	}
+
+
+	return (
+		<>
+		<button onClick={goBack}>
+			Back
+		</button>
+		<h1>Please Login</h1>
+		<h2>{cur}</h2>
+		<button onClick={() => setval(cur + 1)}>
+			Click ME
+		</button>
+		</>
+	);  
 }
 
 
 
 
 function Admin() {
-  return (
-    <>
-      <h1>Welcome {user.name}</h1>
-    </>
-  );
+	return (
+		<>
+		<h2>I LOVE YOU💗💗</h2>
+		</>
+	);
 }
 
 
 
-user = {
-  name: "Shawn",
-  age: 18,
-  isloggedIn: true
-};
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  let content: any;
-  if(user.isloggedIn){
-    content = <Admin />
-  }else{
-    content = <LoginPage />
-  }
+	const [count, setCount] = useState(0)
 
-  
-  if (count == 5){
-    let setCount = () => {count}
-  }
+	let lyrics: string[] = ["Bakit uhaw🥤 sa 'yong sayaw?🕺🏻", "Bakit ikaw?🫵🏻", "'Di bibitaw🫂, sa 'yong sa 'yo", "Laging ikaw", "Ako'y giniginaw, halika rito", "Dito ka lang sa tabi ko", "Mananatiling uhaw💦, uhaw💦, uhaw💦"]
 
-  return (
-    <>
-      <button onClick={setCount}>
+	let content: any;
+	if(user.isloggedIn == true){
+		content = <Admin />
+	}else{
+		content = <LoginPage />
+	}
 
-      </button>
-      {content};
-    </>
-  )
+	let text: string = "Bayot Ka Charles  \n\n\t-Shun";
+
+	if(count != 0){
+		text = lyrics[count-1];
+	}
+	
+	return (
+		<>
+			<h1>
+				{text}
+			</h1>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<button onClick={() => setCount(count + 1)}>
+				Ig click ak
+			</button>
+		</>
+	)
 }
 
 export default App
